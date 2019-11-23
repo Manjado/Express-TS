1314bbf2f910c80e86e3b0bcee40826434145294
malchimowicz <michal.alchimowicz@enovatis.pl>
2019-11-23 12:47:43 +0100

    add GET decorator

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,6 +1,4 @@
 import { Router, Request, Response, NextFunction } from 'express';
-import e = require('express');
-import { request } from 'http';
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
@@ -17,22 +15,6 @@ function requireAuth(req: Request, res: Response, next: NextFunction): void {
 }
 
 const router = Router();
-
-router.get('/login', (req: Request, res: Response) => {
-  res.send(`
-    <form method="POST">
-      <div>
-        <label>Email</label>
-        <input name='email'/>
-      </div>
-      <div>
-        <label>Password</label>
-        <input name='password' type="password" />
-      </div>
-      <button>Submit</button>
-    </form>
-  `);
-});
 
 router.post('/login', (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
